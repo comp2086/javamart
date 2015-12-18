ec60b27a077c30db730b2773e22e672fb8d353c1
FlyingArs <davidyu89tech@gmail.com>
2015-12-18 15:36:19 -0500

    Finished Search Employee and Search Product Panels

--- src/gui/SearchEmployeePanel.java
+++ src/gui/SearchEmployeePanel.java
@@ -4,58 +4,253 @@
  * Handles our search employee panel
  */
 package gui;
-import java.awt.*;
-import javax.swing.*;
 
-public class SearchEmployeePanel extends JPanel
-{
+import db.DBController;
+import hr.Employee;
+import hr.CommissionEmployee.CommissionSalesEmployee;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+public class SearchEmployeePanel extends JPanel {
+
     //components
     //labels
     private final JLabel lblEmployeeSearch, lblEmployeeResult;
-    
+
     //text fields
     private final JTextField txtEmployeeSearch;
-    
+
     //text area
     private final JTextArea araEmployeeResult;
+    private final JScrollPane employeeScrollPane;
     
     //buttons
     private final JButton searchButton, clearAllButton;
-    
-    public SearchEmployeePanel()
-    {
+
+    public SearchEmployeePanel() {
         //set layout
-        setLayout(new GridLayout(3,2));
-        
+        setLayout(new GridLayout(3, 2));
+
         //initialize buttons
         //NOTE: NEED TO ATTACH ACTION LISTENERS RIGHT AFTER BUTTON CREATION
         searchButton = new JButton("Search");
         //SUBMIT BUTTON ACTION LISTENER
-        clearAllButton = new JButton("Clear All");
+        SearchHandler searchHandler = new SearchHandler();
+        searchButton.addActionListener(searchHandler);
         //CLEAR ALL ACTION LISTENER
-        
+        clearAllButton = new JButton("Clear All");
+        ClearHandler clearHandler = new ClearHandler();
+        clearAllButton.addActionListener(clearHandler);
+
         //area to hold buttpns so that we can size them properly
         JPanel buttonPane = new JPanel();
         buttonPane.add(searchButton);
         buttonPane.add(clearAllButton);
-        
+
         //initialize the labels
         lblEmployeeSearch = new JLabel("Search for Employee's:");
         lblEmployeeResult = new JLabel("Employee Result:");
-         
+
         //initialize the text felds
         txtEmployeeSearch = new JTextField(15);
-        
+        txtEmployeeSearch.setName("Employee ID");
+
         //initialize the text area's
-        araEmployeeResult = new JTextArea(3,3);
-        
+        araEmployeeResult = new JTextArea(3, 3);
+        employeeScrollPane = new JScrollPane(araEmployeeResult);
+
         //set the border
         setBorder(BorderFactory.createTitledBorder("Search Employee's"));
-        
+
         add(lblEmployeeSearch);
         add(txtEmployeeSearch);
         add(lblEmployeeResult);
-        add(araEmployeeResult);
+        add(employeeScrollPane);
         add(buttonPane);
     }
+
+    /**
+     * This private internal class implements ActionListener and controls events
+     * relating to the Submit Button click.
+     */
+    Scanner keyboard = new Scanner(System.in);
+    int mainInput, hrInput, productInput;
+
+    private class SearchHandler implements ActionListener {
+
+        //handle the button event
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //declare user varaibles
+            //used to check whether or not we should
+            //display the confirm submission popup
+            boolean checkValid = true;
+
+            MyInputVerifier verifier = new MyInputVerifier();
+
+            //set up a counter to count the number of returned true
+            int counter = 0;
+            //verify each single field
+            if (verifier.verify(txtEmployeeSearch) == false) {
+                checkValid = false;
+            } else if (verifier.verify(txtEmployeeSearch) == true) {
+                counter++;
+            }
+            if (checkValid == true) {
+                displaySearchResult();
+            }//end check valid
+
+        }
+    }//end private Submit Handler class
+
+    //Create an array list to add employee object
+    ArrayList<Employee> employeeList = new ArrayList<>();
+    //ArrayList<Employee>employeeList = DBController.getEmployees();
+
+    //create a test Employee object
+    Employee e1 = new CommissionSalesEmployee(1000, "Donald", "Trump",
+            "Boss", "Trump Tower", "55 Avenue",
+            "666-222-0999", "ZZ333", 0.44);
+
+    /**
+     * Search the input and display them on the textbox
+     */
+    public void displaySearchResult() {
+        int id = 0;
+        try {
+            employeeList.add(e1);
+            //Ask the user to type in the first and last name of the employee
+            //System.out.println("Please Type In the First Name of the Employee");
+            //String firstName = keyboard.nextLine();
+
+            id = Integer.parseInt(txtEmployeeSearch.getText());
+
+            //System.out.println(id);
+            //System.out.println("Please Type In the Last Name of the Employee");
+            //String lastName = keyboard.nextLine();
+
+            //check if the employee list is empty. If yes, send a waring.
+            if (employeeList.isEmpty()) {
+                System.out.println("The Employee List Is Empty. Please Add An Employee");
+
+            } //otherwise, loop through the employee list to search for employee
+            else {
+                //create an isMatch boolean, so when there is no a match within
+                //the list, the warning message is only sent once
+                boolean isMatch = true;
+                if (isMatch) {
+                    for (Employee employee : employeeList) {
+                        System.out.println(employee.getLastName());
+                        System.out.println(employee.getId());
+                        if (id == employee.getId()) {
+                            //System.out.println("\n" + e.toString());
+
+                            araEmployeeResult.setText(employee.toStringFull());
+                            isMatch = true;
+                            break;
+                        } else {
+                            isMatch = false;
+
+                        }
+                    }
+                }
+                if (isMatch == false) //System.out.println("There Is No Match In the System.");  
+                {
+                    JOptionPane.showMessageDialog(null, "There is no match in the system");
+                }
+            }
+        } catch (NumberFormatException fe) {
+            JOptionPane.showMessageDialog(null, "Id is a number. Please enter a new one");
+        }
+    }
+
+    /**
+     * This public internal class inherits from InputVerifier with modifications
+     * specifically designed for the Product Panel.
+     */
+    public class MyInputVerifier extends InputVerifier {
+
+        @Override
+        public boolean verify(JComponent input) {
+            String name = input.getName();
+            String text = ((JTextField) input).getText();
+            if (text.isEmpty()) {
+                JOptionPane.showMessageDialog(null, input.getName() + " cannot be empty");
+                return false;
+            } else if (name == "Commission Rate") {
+                try {
+
+                    double tempCommissionRate = Double.parseDouble(text);
+
+                    if (tempCommissionRate < 0 || tempCommissionRate > 1) {
+                        JOptionPane.showMessageDialog(null, "Commission rate must be a percent (enter value between 0 and 1).");
+                        return false;
+                    }
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,
+                            "This is not a valid commission rate. Please try again.");
+                    return false;
+                }
+                return true;
+            }//end commission rate validator
+            else if (name == "First Name" || name == "Last Name") {
+
+                if (text.length() > 25) {
+                    JOptionPane.showMessageDialog(null,
+                            "Names must not exceed 25 characters.");
+                    return false;
+                }
+                return true;
+
+            }//end name validator
+            else if (name == "Address") {
+
+                if (text.length() > 100) {
+                    JOptionPane.showMessageDialog(null,
+                            "Address must not exceed 100 characters.");
+                    return false;
+                }
+                return true;
+            }//end address validator
+            else if (name == "Sin") {
+
+                if (!(text.length() == 9)) {
+                    JOptionPane.showMessageDialog(null,
+                            "SIN must be 9 characters.");
+                    return false;
+                }
+                return true;
+            }//end SIN validator
+            else if (name == "Phone") {
+
+                if (text.length() > 12) {
+                    JOptionPane.showConfirmDialog(null,
+                            "Phone number must not exceed 12 digits.");
+                    return false;
+                }
+                return true;
+            }//end phone validator
+            else {
+                return true;
+            }
+
+        }//verify method
+    }//end verifier class
+
+    /**
+     * This private internal class implements ActionListener to control events
+     * relating to the Clear Button click.
+     */
+    private class ClearHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            txtEmployeeSearch.setText("");
+            araEmployeeResult.setText("");
+
+        }//end action handler
+    }//end clear handler
 }
--- src/gui/SearchProductPanel.java
+++ src/gui/SearchProductPanel.java
@@ -4,8 +4,16 @@
  * Handles our search product panel
  */
 package gui;
+
+import db.DBController;
+import hr.Employee;
+import hr.CommissionEmployee.CommissionSalesEmployee;
+import inventory.Manufacturer;
+import inventory.Product;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
 
 public class SearchProductPanel extends JPanel
 {
@@ -18,6 +26,7 @@ public class SearchProductPanel extends JPanel
     
     //text area
     private final JTextArea araProductResult;
+    private final JScrollPane productScrollPane;
     
     //buttons
     private final JButton searchButton, clearAllButton;
@@ -30,9 +39,14 @@ public class SearchProductPanel extends JPanel
         //initialize buttons
         //NOTE: NEED TO ATTACH ACTION LISTENERS RIGHT AFTER BUTTON CREATION
         searchButton = new JButton("Search");
-        //SUBMIT BUTTON ACTION LISTENER
-        clearAllButton = new JButton("Clear All");
+        SearchHandler searchHandler = new SearchHandler();
+        searchButton.addActionListener(searchHandler);
+        //Search BUTTON ACTION LISTENER
+                
         //CLEAR ALL ACTION LISTENER
+        clearAllButton = new JButton("Clear All");
+        ClearHandler clearHandler = new ClearHandler();
+        clearAllButton.addActionListener(clearHandler);
         
         //area to hold buttpns so that we can size them properly
         JPanel buttonPane = new JPanel();
@@ -45,9 +59,11 @@ public class SearchProductPanel extends JPanel
          
         //initialize the text felds
         txtProductSearch = new JTextField(15);
+        txtProductSearch.setName("Product ID");
         
         //initialize the text area's
         araProductResult = new JTextArea(3,3);
+        productScrollPane = new JScrollPane(araProductResult);
         
         //set the border
         setBorder(BorderFactory.createTitledBorder("Search Product's"));
@@ -55,7 +71,189 @@ public class SearchProductPanel extends JPanel
         add(lblProductSearch);
         add(txtProductSearch);
         add(lblProductResult);
-        add(araProductResult);
+        add(productScrollPane);
         add(buttonPane);
     }
+    /**
+     * This private internal class implements ActionListener and controls events
+     * relating to the Submit Button click.
+     */
+    Scanner keyboard = new Scanner(System.in);
+    int mainInput, hrInput, productInput;
+
+    private class SearchHandler implements ActionListener {
+
+        //handle the button event
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //declare user varaibles
+            //used to check whether or not we should
+            //display the confirm submission popup
+            boolean checkValid = true;
+
+            MyInputVerifier verifier = new MyInputVerifier();
+
+            //set up a counter to count the number of returned true
+            int counter = 0;
+            //verify each single field
+            if (verifier.verify(txtProductSearch) == false) {
+                checkValid = false;
+            } else if (verifier.verify(txtProductSearch) == true) {
+                counter++;
+            }
+            if (checkValid == true) {
+                displaySearchResult();
+            }//end check valid
+
+        }
+    }//end private Submit Handler class
+
+    //Create an array list to add employee object
+    ArrayList<Product> productList = new ArrayList<>();
+    //ArrayList<Product>productList = DBController.getProducts();
+
+    //create a test Employee object
+    Manufacturer manu = new Manufacturer("Georgian Manufacturer");
+    Product p1 = new Product(1000, "Carrot", "Red", "Z1000",
+                  "25", "30", true, manu);
+
+    /**
+     * Search the input and display them on the textbox
+     */
+    public void displaySearchResult() {
+        int id = 0;
+        try {
+            productList.add(p1);
+            //Ask the user to type in the first and last name of the employee
+            //System.out.println("Please Type In the First Name of the Employee");
+            //String firstName = keyboard.nextLine();
+
+            id = Integer.parseInt(txtProductSearch.getText());
+
+            //System.out.println(id);
+            //System.out.println("Please Type In the Last Name of the Employee");
+            //String lastName = keyboard.nextLine();
+
+            //check if the employee list is empty. If yes, send a waring.
+            if (productList.isEmpty()) {
+                System.out.println("The Product List Is Empty. Please Add A Product");
+
+            } //otherwise, loop through the employee list to search for employee
+            else {
+                //create an isMatch boolean, so when there is no a match within
+                //the list, the warning message is only sent once
+                boolean isMatch = true;
+                if (isMatch) {
+                    for (Product product : productList) {
+                        System.out.println(product.getName());
+                        System.out.println(product.getId());
+                        if (id == product.getId()) {
+                            //System.out.println("\n" + e.toString());
+
+                            araProductResult.setText(product.toStringFull());
+                            isMatch = true;
+                            break;
+                        } else {
+                            isMatch = false;
+
+                        }
+                    }
+                }
+                if (isMatch == false) //System.out.println("There Is No Match In the System.");  
+                {
+                    JOptionPane.showMessageDialog(null, "There is no match in the system");
+                }
+            }
+        } catch (NumberFormatException fe) {
+            JOptionPane.showMessageDialog(null, "Id is a number. Please enter a new one");
+        }
+    }
+
+    /**
+     * This public internal class inherits from InputVerifier with modifications
+     * specifically designed for the Product Panel.
+     */
+    public class MyInputVerifier extends InputVerifier {
+
+        @Override
+        public boolean verify(JComponent input) {
+            String name = input.getName();
+            String text = ((JTextField) input).getText();
+            if (text.isEmpty()) {
+                JOptionPane.showMessageDialog(null, input.getName() + " cannot be empty");
+                return false;
+            } else if (name == "Commission Rate") {
+                try {
+
+                    double tempCommissionRate = Double.parseDouble(text);
+
+                    if (tempCommissionRate < 0 || tempCommissionRate > 1) {
+                        JOptionPane.showMessageDialog(null, "Commission rate must be a percent (enter value between 0 and 1).");
+                        return false;
+                    }
+
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null,
+                            "This is not a valid commission rate. Please try again.");
+                    return false;
+                }
+                return true;
+            }//end commission rate validator
+            else if (name == "First Name" || name == "Last Name") {
+
+                if (text.length() > 25) {
+                    JOptionPane.showMessageDialog(null,
+                            "Names must not exceed 25 characters.");
+                    return false;
+                }
+                return true;
+
+            }//end name validator
+            else if (name == "Address") {
+
+                if (text.length() > 100) {
+                    JOptionPane.showMessageDialog(null,
+                            "Address must not exceed 100 characters.");
+                    return false;
+                }
+                return true;
+            }//end address validator
+            else if (name == "Sin") {
+
+                if (!(text.length() == 9)) {
+                    JOptionPane.showMessageDialog(null,
+                            "SIN must be 9 characters.");
+                    return false;
+                }
+                return true;
+            }//end SIN validator
+            else if (name == "Phone") {
+
+                if (text.length() > 12) {
+                    JOptionPane.showConfirmDialog(null,
+                            "Phone number must not exceed 12 digits.");
+                    return false;
+                }
+                return true;
+            }//end phone validator
+            else {
+                return true;
+            }
+
+        }//verify method
+    }//end verifier class
+
+    /**
+     * This private internal class implements ActionListener to control events
+     * relating to the Clear Button click.
+     */
+    private class ClearHandler implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            txtProductSearch.setText("");
+            araProductResult.setText("");
+
+        }//end action handler
+    }//end clear handler
 }
